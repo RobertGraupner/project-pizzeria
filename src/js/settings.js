3c57071ce394d794bce00912d215f3ff0fb73d69
Robert Graupner <robert.graupnerr@gmail.com>
2022-01-19 11:27:22 +0100

    add heroku

--- src/js/settings.js
+++ src/js/settings.js
@@ -118,7 +118,7 @@ export const settings = {
   },
   // kod dodany do komunikacji z API
   db: {
-    url: '//localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname == 'localhost' ? ':3131' : ''),
     products: 'products',
     orders: 'orders',
     bookings: 'bookings',
